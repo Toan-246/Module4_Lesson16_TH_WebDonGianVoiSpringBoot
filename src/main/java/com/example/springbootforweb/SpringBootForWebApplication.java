6feffd4e1879f39eb40e5b29fac9394079216fcb
Toan-246 <n.v.toan687@gmail.com>
2022-04-07 10:07:21 +0700

    first commit

---
+++ src/main/java/com/example/springbootforweb/SpringBootForWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootforweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootForWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootforweb/SpringBootForWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootforweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
